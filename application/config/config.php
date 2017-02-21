a6d0dc562ce1ce02f72277a9a80e05b51afba608
AlexisGaleas94 <alexis.galeas93@gmail.com>
2017-02-20 20:28:54 -0600

    cambio de ruta
    cambio de ruta de carpeta

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 //$config['base_url'] = 'http://enloszapatosdelotro.com.mx/';
-$config['base_url'] = 'http://localhost/zapa/';
+$config['base_url'] = 'http://localhost/elzdo/';
 
 /*
 |--------------------------------------------------------------------------
